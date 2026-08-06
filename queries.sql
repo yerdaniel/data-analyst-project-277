8c9b73ca55160ad308830e7ca89bd3c017d3512d
Yerson_Diaz <yer25tan@gmail.com>
2026-08-05 21:22:44 -0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,8 +1,8 @@
---Consulta para contar el numero total de clientes registrados en la tabla customers
+-- Consulta para contar el numero total de clientes registrados en la tabla customers
 SELECT COUNT(*) AS customers_count
 FROM customers;
 
---Reporte de los 10 vendedores con mayor ingreso generado por sus ventas
+-- Reporte de los 10 vendedores con mayor ingreso generado por sus ventas
 select 
 e.first_name ||' '|| e.last_name as seller,
 SUM(s.quantity) as operations,
@@ -15,3 +15,24 @@ from sales as s
 group by sales_person_id , first_name,  last_name
 order by income desc  
 limit 10; 
+
+
+
+with tab1 as (
+select 
+AVG(s.quantity*p.price) as promgen
+from sales as s 
+	inner join products as p
+		on s.product_id = p.product_id)
+		
+select 
+e.first_name ||' '|| e.last_name as seller,
+FLOOR(AVG(s.quantity*p.price)) as average_income
+from sales as s 
+	inner join products as p
+		on s.product_id = p.product_id
+			inner join employees as e 
+				on e.employee_id=s.sales_person_id
+group by sales_person_id, first_name, last_name
+having FLOOR(AVG(s.quantity*p.price)) < (select promgen from tab1)
+order by average_income asc;
